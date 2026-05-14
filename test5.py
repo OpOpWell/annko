5a34aaff7d8f3df5e3b57734913b1458a838cdeb
user <user@example.com>
2026-05-14 15:37:35 +0900

    OCR結果をデキスパートCSV出力

--- test5.py
+++ test5.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pytesseract
 import re
+import pandas as pd
 import os
 from openpyxl import load_workbook
 
@@ -25,8 +26,10 @@ for filename in os.listdir(image_folder):
 
         img = Image.open(image_file)
         img = img.convert("L")
+
         debug_file = "確認_" + filename
         img.save(debug_file)
+
         print("確認画像保存:", debug_file)
 
         text = pytesseract.image_to_string(
@@ -38,10 +41,15 @@ for filename in os.listdir(image_folder):
         print("OCR結果")
         print(text)
 
-        raw_values = re.findall(r"[0-9]?\.[0-9]{2,3}|[0-9]{4}", text)
+        raw_values = re.findall(
+            r"[0-9]?\.[0-9]{2,3}|[0-9]{4}",
+            text
+        )
 
         print("抽出数値")
         print(raw_values)
+
+        image_values = []
 
         for v in raw_values:
             if v.startswith("."):
@@ -50,15 +58,18 @@ for filename in os.listdir(image_folder):
             elif "." not in v and len(v) == 4:
                 v = str(float(v) / 1000)
 
-            values.append(float(v))
             num = float(v)
 
-if 1.3 <= num <= 1.5:
-    values.append(num)
+            if 1.3 <= num <= 1.5:
+                values.append(num)
+                image_values.append(num)
+
+        print(filename, "→", image_values)
 
 print("復元値")
 print(values)
 
+# Excel入力
 wb = load_workbook(input_file)
 ws = wb["測定結果表"]
 
@@ -67,9 +78,31 @@ target_col = "C"
 
 for i, value in enumerate(values):
     row = start_row + i * 2
+
     ws[f"{target_col}{row}"] = value
+
     print(f"{target_col}{row} = {value}")
 
 wb.save(output_file)
 
 print("保存しました:", output_file)
+
+# CSV出力
+csv_data = []
+
+for i, value in enumerate(values):
+    csv_data.append([
+        f"GE-{i+1}",
+        value
+    ])
+
+df = pd.DataFrame(csv_data)
+
+df.to_csv(
+    "dekispart.csv",
+    index=False,
+    header=False,
+    encoding="cp932"
+)
+
+print("CSV保存完了: dekispart.csv")
